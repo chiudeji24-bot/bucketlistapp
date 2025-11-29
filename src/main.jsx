95e01653f015d045b3f98fc9bbbc67df8ec7f1b6
Chi <chiudeji24@gmail.com>
2025-11-28 21:19:26 -0800

    Final: correct main.jsx + working config

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
+import './index.css'
 import { Amplify } from 'aws-amplify'
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import outputs from '../amplify_outputs.json'
 
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: 'us-west-2_eV1NeyZ55',
-      userPoolClientId: '5hhvaf1kctcm63im4fvlhvuchj',
-    }
-  }
-});
-
-function Root() {
-  return <App />
-}
-
-const AppWithAuth = withAuthenticator(Root)
+Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppWithAuth />
+    <App />
   </React.StrictMode>
 )
+  
